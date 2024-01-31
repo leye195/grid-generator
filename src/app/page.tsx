6861ef5f57cb0bdbdd7aa5ef314dd6b25e7293c8
leye195 <leye195@naver.com>
2024-01-31 22:38:47 +0900

    style: set paddingLeft to 4px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-10 pl-10">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-10 pl-1">
       <Suspense>
         <GridBox />
       </Suspense>
