e2a1271bd510ffeab4cf5e15f9689846dbd73723
leye195 <leye195@naver.com>
2024-01-31 22:31:44 +0900

    style: adjust padding on main tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-10 pl-10">
       <Suspense>
         <GridBox />
       </Suspense>
